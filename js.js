a5857c75efd22ed2b5ce4bd8b93f73e95fdf9c59
Maranaho <maranaho.nguessan@gmail.com>
2018-01-11 17:53:42 +0000

    fix the form

--- js.js
+++ js.js
@@ -1,19 +1,47 @@
+
 (function() {
 
-  let data = ['Marshall','Beaner'];
+  let data = [
+  {
+    label: 'Styleguide creation',
+    done: false
+  },
+  {
+    label: 'Send wireframe',
+    done: true
+  },
+  {
+    label: 'Readability About page',
+    done: false
+  },
+  {
+    label: 'Check color contrast',
+    done: false
+  }];
 
   const form = document.getElementById('form');
   const list = document.getElementById('list');
   const input = document.getElementById('input');
+  const revealForm = document.getElementById('revealForm');
+  const btn_ctn = document.getElementById('btn_ctn');
 
   form.addEventListener('submit',addTodo);
-  list.addEventListener('click',remove); //Set to remove to remove li
+  list.addEventListener('click',markAsDone); //Set to remove to remove li
+  revealForm.addEventListener('click',formRevealer);
 
 
   function init() {
     populateList();
+
+    for (var i = 0; i < data.length; i++) {
+      addDoneClass(list.children[i],true,i);
+    }
   }
 
+
+  function formRevealer() {
+    btn_ctn.classList = 'addItem';
+  }
 
 
   function remove(e) {
@@ -22,26 +50,47 @@
     populateList();
   }
 
+  function addDoneClass(elt,bool,idx) {
+
+      if (bool) {
+        if (data[idx].done) {
+          elt.classList = 'done';
+        } else {
+          elt.classList = '';
+        }
+      } else {
+        if (data[idx].done) {
+          elt.classList = '';
+        } else {
+          elt.classList = 'done';
+        }
+        data[idx].done = !data[idx].done;
+      }
+
+  }
+
+
+
 
   function markAsDone(e) {
-    let li = e.target.parentNode;
-    let isTodo = li.classList.length === 0;
-    if (isTodo) {
-      li.classList = 'isDone';
-    } else {
-      li.classList = '';
-    }
+    let current = e.target;
+    let idx = Number(current.getAttribute('key'));
+    addDoneClass(current.parentNode,false,idx);
+
   }
 
   function addTodo(e) {
     e.preventDefault();
-    let newTodo = input.value;
-    if (newTodo === '') { return }
+    let newTodo = {
+      label: input.value,
+      done: false
+    };
+    if (newTodo.label === '') { return }
     let currentList = data;
     currentList.push(newTodo);
     data = currentList;
     input.value = '';
-    populateList(currentList);
+    init();
   }
 
   function populateList() {
@@ -50,7 +99,7 @@
       let singleTodo = document.createElement('li');
       let liSpan = document.createElement('span');
       let btn = document.createElement('button');
-      liSpan.innerText = data[i];
+      liSpan.innerText = data[i].label;
       singleTodo.append(liSpan);
       btn.innerText = 'Remove';
       btn.setAttribute('key',i);
